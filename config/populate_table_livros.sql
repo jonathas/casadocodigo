f168affc1c5e7e185b2c7ecb174ed6a95747460d
Jon Ribeiro <contact@jonathas.com>
2016-08-07 23:18:53 +0200

    Adicionado arquivo sql para popular a tabela livros

---
+++ config/populate_table_livros.sql
@@ -0,0 +1,7 @@
+insert into livros
+	(titulo,descricao,preco) 
+VALUES 
+	('Comecando com nodejs', 'livro sobre nodejs', 40.00),
+	('Comecando com javascript', 'livro sobre javascript', 40.00),
+	('Comecando com express', 'livro sobre express', 40.00),
+	('Indo alem com javascript', 'javascript avancado', 39.90);
