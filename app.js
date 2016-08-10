6d6328d27cd33206e49bdc39dedf823467a01cad
Jon Ribeiro <jonathas.ribeiro@mautilus.com>
2016-08-10 13:46:28 +0200

    Configurado deploy para o Heroku

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ var io = require('socket.io')(http);
 // Para que fique disponível para todas as rotas
 app.set('io',io);
 
+// Utilizando valor da porta setado no ambiente ou 3000 se não estiver setada
+var porta = process.env.PORT || 3000;
+
 // Mudado de app para http por causa do Socket.io
-http.listen(3000, function() {
-    console.log("Servidor está rodando");
+http.listen(porta, function() {
+    console.log("Servidor está rodando no heroku");
 });
