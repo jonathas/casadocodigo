c762c12e3fe3fa6fd95309e62a679bcd0102cb42
Jon Ribeiro <contact@jonathas.com>
2016-08-10 22:21:45 +0200

    De qualquer forma, quando ha um erro de conexao, connection nao esta definida. Entao, removendo do codigo

--- app/infra/ProdutosDAO.js
+++ app/infra/ProdutosDAO.js
@@ -5,12 +5,8 @@ function ProdutosDAO(app) {
 ProdutosDAO.prototype.lista = function(callback) {
     this._app.infra.connectionFactory(function(err, connection) {
         if (err) {
-            if(connection) {
-                connection.release();
-            }
 	  		console.log(' Error getting mysql_pool connection: ' + err);
 	  		throw err;
-            return;
 	  	}
         connection.query('select * from livros', function(erros, results) {
             connection.release();
@@ -22,12 +18,8 @@ ProdutosDAO.prototype.lista = function(callback) {
 ProdutosDAO.prototype.salva = function(produto, callback) {
     this._app.infra.connectionFactory(function(err, connection) {
         if (err) {
-            if(connection) {
-                connection.release();
-            }
 	  		console.log(' Error getting mysql_pool connection: ' + err);
 	  		throw err;
-            return;
 	  	}
         connection.query('insert into livros set ?', produto, function(erros, results) {
             connection.release();
