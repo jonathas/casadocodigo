10ce67d3acd9f6922992a004c76ca38ac02c1861
Jon Ribeiro <jonathas.ribeiro@mautilus.com>
2016-08-10 10:18:50 +0200

    Implementada comunicação via Socket.io

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var app = require('./config/express')();
 
-app.listen(3000, function() {
+// Configurando o Socket.io
+var http = require('http').Server(app); //Servidor do Node.js
+var io = require('socket.io')(http);
+
+// Para que fique disponível para todas as rotas
+app.set('io',io);
+
+// Mudado de app para http por causa do Socket.io
+http.listen(3000, function() {
     console.log("Servidor está rodando");
 });
